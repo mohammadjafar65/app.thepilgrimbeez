6520411f6fa1e95dc54ff8816185d36ab1a672d7
Mohammad Jafar <itzafar65@gmail.com>
2024-02-22 00:38:04 +0530

    update_6

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,14 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+// Middleware to set CORS headers
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://admin.thepilgrimbeez.com');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 require('dotenv').config();
 
 // Define the port
