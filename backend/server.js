85b88ff26b737e50505368ea0f0a94a863d0bc51
Mohammad Jafar <itzafar65@gmail.com>
2024-02-22 01:18:42 +0530

    update_11

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ app.use((req, res, next) => {
 require('dotenv').config();
 
 // Define the port
-const port = process.env.PORT || 3301;
+const port = process.env.PORT || 3302;
 
 // Database connection
 const db = mysql.createConnection({
