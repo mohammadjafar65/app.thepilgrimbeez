7664ab8694805f6ee796ff4c78b3a54e272a8e7e
Mohammad Jafar <itzafar65@gmail.com>
2024-02-22 01:45:05 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,11 +8,12 @@ const { v4: uuidv4 } = require("uuid");
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: 'https://admin.thepilgrimbeez.com', // Allow requests from this origin
+}));
 
 // Middleware to set CORS headers
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*'); // Allow requests from any origin
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
