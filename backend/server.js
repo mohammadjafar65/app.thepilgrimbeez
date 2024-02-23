5a591b1b74fdfc4ca1a564941cc3eb528273b757
Mohammad Jafar <itzafar65@gmail.com>
2024-02-23 12:26:28 +0530

    update_26

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,19 @@
 const express = require('express');
-const cors = require('cors'); // Import the cors module
+const cors = require('cors');
 const multer = require("multer");
 const fs = require("fs");
 const mysql = require("mysql");
 const path = require("path");
 const { v4: uuidv4 } = require("uuid");
 
-const app = express(); // Initialize Express app
+const app = express();
 app.use(cors());
+app.use(express.json()); // Add this line to parse JSON requests
 
 require('dotenv').config();
 
-// Define the port
-const port = process.env.PORT || 3007;
+const port = process.env.PORT || 3008;
 
-// Database connection
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
@@ -30,7 +29,6 @@ db.connect(err => {
   console.log('Connected to database.');
 });
 
-// Multer configuration for file uploads
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, 'uploads/')
@@ -42,23 +40,23 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage });
 
-// Ensure the uploads directory exists
 const uploadDir = path.join(__dirname, "uploads");
 if (!fs.existsSync(uploadDir)) {
   fs.mkdirSync(uploadDir, { recursive: true });
 }
 
-// Serve static files from the uploads directory
 app.use("/uploads", express.static("uploads"));
 app.use("/static", express.static(path.join(__dirname, "public")));
 
-// Routes for travel packages
-require('./travelPackages')(app, db, upload, uuidv4);
+// Middleware to handle errors globally
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something went wrong!');
+});
 
-// Routes for student
+require('./travelPackages')(app, db, upload, uuidv4);
 require('./users')(app, db);
 
-// Start the server
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
